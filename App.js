e4e3520b597fc5e3927d65ed27472088b501ad5d
Fujie Chen <imaple7@gmail.com>
2018-02-02 22:53:02 -0600

    Show the hot post and author

--- App.js
+++ App.js
@@ -1,23 +1,99 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { 
+  AppRegistry,
+  FlatList, 
+  StyleSheet, 
+  Text, 
+  View,
+  ActivityIndicator,
+  Platform,
+  Alert
+} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
+export default class RedditHotPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://www.reddit.com/r/canada/hot.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.data.children,
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  FlatListItemSeparator = () => {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+      />
+    );
+  }
+
+  GetFlatListItem (author_name) {
+    Alert.alert("Author is " + author_name);
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      // <View style={{flex: 1, padding:20}}>
+      //   <ListView
+      //     dataSource={this.state.dataSource}
+      //     renderRow={(rowData) => 
+      //       <Text>{rowData.data.title}, By {rowData.data.author}</Text>}
+      //   />
+      // </View>
+
+      <View style={styles.MainContainer}>
+        <FlatList
+          data={ this.state.dataSource }
+          ItemSeparatorComponent = {this.FlatListItemSeparator}
+          renderItem={({item}) => <Text style={styles.FlatListItemStyle} onPress={this.GetFlatListItem.bind(this, item.data.author)} > {item.data.title} </Text>}
+          keyExtractor={(item, index) => index}
+          />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+
+  MainContainer :{
     justifyContent: 'center',
+    flex:1,
+    margin: 10,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
   },
+
+  FlatListItemStyle: {
+    padding: 10,
+    fontSize: 18,
+    //height: 44,
+  },
+
 });
